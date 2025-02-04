36583b900bbc104bad4cd64711b3b405aa67ff7c
𝐀𝐋𝐈ャ <ar6167423@gmail.com>
2025-02-04 12:35:55 +0500

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -23,7 +23,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `*╭──────────●●►*
-*KERM_MD-V4 UPTIME LIST↷*
+*ALI_MD-V4 UPTIME LIST↷*
 
 *_UPTIME:➠_*  ${runtime(process.uptime())}
 
@@ -31,7 +31,7 @@ let status = `*╭──────────●●►*
 
 *_HOSTNAME:➠_* ${os.hostname()}
 
-*_OWNER:➠_* *KG TECH*
+*_OWNER:➠_* *𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪*
 *╰──────────●●►*
 `
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
