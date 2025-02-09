7df485c07780e08de07a7457cc348aab2544fd02
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-02-09 16:21:13 +0500

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -1,15 +1,4 @@
-/*
-_  ______   _____ _____ _____ _   _
-| |/ / ___| |_   _| ____/___ | | | |
-| ' / |  _    | | |  _|| |   | |_| |
-| . \ |_| |   | | | |__| |___|  _  |
-|_|\_\____|   |_| |_____\____|_| |_|
 
-ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
-CONTACT ME HERE +237656520674
-YT: KermHackTools
-Github: Kgtech-cmr
-*/
 
 const axios = require('axios');
 const { cmd } = require('../command');
@@ -28,7 +17,7 @@ async (conn, mek, m, { from, reply }) => {
         const message = `
 💬 "${quote.content}"
 - ${quote.author}
-*QUOTES BY KG TECH*
+*QUOTES BY ALI TECH*
         `;
         return reply(message);
     } catch (e) {
