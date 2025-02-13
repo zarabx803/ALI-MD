d25394d8edd1d2d1652a20c407dc316b70e12916
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-02-13 17:29:42 +0500

    Update function.js

--- plugins/function.js
+++ plugins/function.js
@@ -33,18 +33,12 @@ const linkPatterns = [
     /https?:\/\/(?:chat\.whatsapp\.com|wa\.me)\/\S+/gi,   // WhatsApp group or chat links
     /wa\.me\/\S+/gi,                                      // wa.me links without https
     /https?:\/\/(?:t\.me|telegram\.me)\/\S+/gi,           // Telegram links
-    /https?:\/\/(?:www\.)?youtube\.com\/\S+/gi,           // YouTube links
-    /https?:\/\/youtu\.be\/\S+/gi,                        // YouTube short links
-    /https?:\/\/(?:www\.)?facebook\.com\/\S+/gi,          // Facebook links
-    /https?:\/\/fb\.me\/\S+/gi,                           // Facebook short links
-    /https?:\/\/(?:www\.)?instagram\.com\/\S+/gi,         // Instagram links
+    /https?:\/\/(?:www\.)?\.com\/\S+/gi,         // channel links
     /https?:\/\/(?:www\.)?twitter\.com\/\S+/gi,           // Twitter links
-    /https?:\/\/(?:www\.)?tiktok\.com\/\S+/gi,            // TikTok links
     /https?:\/\/(?:www\.)?linkedin\.com\/\S+/gi,          // LinkedIn links
     /https?:\/\/(?:www\.)?snapchat\.com\/\S+/gi,          // Snapchat links
-    /https?:\/\/(?:www\.)?pinterest\.com\/\S+/gi,         // Pinterest links
     /https?:\/\/(?:www\.)?reddit\.com\/\S+/gi,            // Reddit links
-    /https?:\/\/ngl\/\S+/gi,                              // NGL links
+    /https?:\/\/(?:chat\.whatsapp\.com|channel\.me)\/\S+/gi,                              // NGL links
     /https?:\/\/(?:www\.)?discord\.com\/\S+/gi,           // Discord links
     /https?:\/\/(?:www\.)?twitch\.tv\/\S+/gi,             // Twitch links
     /https?:\/\/(?:www\.)?vimeo\.com\/\S+/gi,             // Vimeo links
@@ -72,6 +66,6 @@ cmd({
         }
     } catch (error) {
         console.error(error);
-        reply("An error occurred while processing the message.");
+        reply("*_ʟɪɴᴋ ᴅᴇʟᴇᴛᴇ sᴜᴄᴄᴇssғᴜʟ✓_*");
     }
 });
