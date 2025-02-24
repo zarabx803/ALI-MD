5ba8465219f7d53712fa44c7a6ca62983447f47a
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-02-25 00:56:33 +0500

    Update owner1.js

--- plugins/owner1.js
+++ plugins/owner1.js
@@ -93,7 +93,7 @@ async (conn, mek, m, { from, isOwner, quoted, reply }) => {
 });
 // 6. Clear All Chats
 cmd({
-    pattern: "clearchats",
+    pattern: "clear",
     desc: "Clear all chats from the bot.",
     category: "owner",
     react: "🧹",
@@ -112,32 +112,6 @@ async (conn, mek, m, { from, isOwner, reply }) => {
     }
 });
 
-
-cmd({
-    pattern: "jid",
-    desc: "Get the bot's JID.",
-    category: "owner",
-    react: "🤖",
-    filename: __filename
-},
-async (conn, mek, m, { from, isOwner, reply }) => {
-    if (!isOwner) return reply("❌ You are not the owner!");
-    reply(`🤖 *Bot JID:* ${conn.user.jid}`);
-});
-// 8. Group JIDs List
-cmd({
-    pattern: "gjid",
-    desc: "Get the list of JIDs for all groups the bot is part of.",
-    category: "owner",
-    react: "📝",
-    filename: __filename
-},
-async (conn, mek, m, { from, isOwner, reply }) => {
-    if (!isOwner) return reply("❌ You are not the owner!");
-    const groups = await conn.groupFetchAllParticipating();
-    const groupJids = Object.keys(groups).join('\n');
-    reply(`📝 *Group JIDs:*\n\n${groupJids}`);
-});
 // delete 
 
 cmd({
