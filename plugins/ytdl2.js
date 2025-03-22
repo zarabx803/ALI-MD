095bd1673952029c5ce864b3afb762d8a4198337
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-03-22 18:41:50 +0500

    𓆩ု᪳𝐀𝐋𝐈-𝐌𝐃ှ᪳𓆪

--- plugins/ytdl2.js
+++ plugins/ytdl2.js
@@ -86,11 +86,11 @@ const yt = await ytsearch(q);
     let response = await fetch(apiUrl);
     let data = await response.json();
     
-    if (data.status !== 200 || !data.success || !data.result.downloadUrl) {
+    if (data.status !== 300 || !data.success || !data.result.downloadUrl) {
         return reply("Failed to fetch the audio. Please try again later.");
     }
     
-    let ytmsg = `*PLAYING: ${yts.title}*\n> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀʟɪ-ᴍᴅ🫟*`;
+    let ytmsg = `*PLAYING: ${yts.title}*`;
 
 
 
