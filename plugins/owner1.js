5a2b60b9eff4faf0c63c588d86093687dcf02af8
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-04-25 01:40:51 +0500

    @itx-alii-raza

--- plugins/owner1.js
+++ plugins/owner1.js
@@ -74,31 +74,3 @@ async (conn, mek, m, { from, isOwner, reply }) => {
         reply(`❌ Error clearing chats: ${error.message}`);
     }
 });
-
-// delete 
-
-cmd({
-pattern: "delete",
-react: "🧹",
-alias: ["del"],
-desc: "delete message",
-category: "group",
-use: '.del',
-filename: __filename
-},
-async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-if (!isOwner ||  !isAdmins) return;
-try{
-if (!m.quoted) return reply(mg.notextfordel);
-const key = {
-            remoteJid: m.chat,
-            fromMe: false,
-            id: m.quoted.id,
-            participant: m.quoted.sender
-        }
-        await conn.sendMessage(m.chat, { delete: key })
-} catch(e) {
-console.log(e);
-reply('successful..👨‍💻✅')
-} 
-})
