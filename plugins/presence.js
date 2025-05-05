1b3e556286f2fe1e30ff5464ba8547a72eba3f2d
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-05-06 01:23:23 +0500

    @itx-alii-raza

--- plugins/presence.js
+++ plugins/presence.js
@@ -108,28 +108,3 @@ async (conn, mek, m, { from, body, isOwner }) => {
         await conn.sendPresenceUpdate('composing', from); // send typing 
     }
 });
-
-// Public Mod
-
-cmd({
-  on: "body"
-}, async (conn, mek, m, { from, isOwner }) => {
-  try {
-    if (config.ALWAYS_ONLINE === 'true') {
-      // Public Mode + Always Online: Always show as online
-      await conn.sendPresenceUpdate("available", from);
-    } else if (config.PUBLIC_MODE === 'true') {
-      // Public Mode + Dynamic: Respect owner's presence
-      if (isOwner) {
-        // If owner is online, show available
-        await conn.sendPresenceUpdate("available", from);
-      } else {
-        // If owner is offline, show unavailable
-        await conn.sendPresenceUpdate("unavailable", from);
-      }
-    }
-  } catch (e) {
-    console.log(e);
-  }
-});
-              
